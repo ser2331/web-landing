e9d44afe67301202f8223119a5d2b8cb2f57b85d
Dukov <dukov_serega@mail.ru>
2023-04-03 23:14:01 +0300

    some fix payment links

--- src/range-input.js
+++ src/range-input.js
@@ -1,5 +1,4 @@
 for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
-  console.log(e);
   e.style.setProperty('--value', e.value);
   e.style.setProperty('--min', e.min == '' ? '0' : e.min);
   e.style.setProperty('--max', e.max == '' ? '100' : e.max);
@@ -8,9 +7,11 @@ for (let e of document.querySelectorAll('input[type="range"].slider-progress')) 
 
 const rangeInput = document.querySelector('input[type="range"].slider-progress.revenue-range');
 const rangeInputValueShower = document.querySelector('.revenue-range-value');
-const rangeInputValueShowerVal = rangeInputValueShower.querySelector('.value');
-rangeInput.addEventListener('input', (e) => {
-  const widthInput = rangeInput.getBoundingClientRect().width;
-  rangeInputValueShower.style.setProperty('--transformRange', (widthInput / 250) * rangeInput.value + 'px');
-  rangeInputValueShowerVal.innerHTML = rangeInput.value;
+const rangeInputValueShowerVal = rangeInputValueShower?.querySelector('.value');
+rangeInput?.addEventListener('input', (e) => {
+  const widthInput = rangeInput?.getBoundingClientRect().width;
+  rangeInputValueShower?.style.setProperty('--transformRange', (widthInput / 250) * rangeInput.value + 'px');
+  if (rangeInputValueShowerVal) {
+    rangeInputValueShowerVal.innerHTML = rangeInput.value;
+  }
 });
